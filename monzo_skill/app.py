fb8a8f6be33a946599a2dd4aa102cde1e19cb6f0
Matt Pye <pyematt@gmail.com>
2016-10-11 22:30:12 +0100

    Hook in Mondo API calls for IntentRequest

--- monzo_skill/app.py
+++ monzo_skill/app.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, request, jsonify
 
-# from monzo import MonzoClient
+from .utils import (
+    get_balance, get_spent_today, get_last_transaction,
+    build_speech_response, build_linked_account, build_response
+)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'changeme'
@@ -18,8 +23,10 @@ def alexa():
             'amzn1.ask.skill.98d21241-9161-42af-a29b-5cf778c12357'):
         raise ValueError('Invalid Application ID')
 
-    # If not acccess code
-        # Reply with session request - build_linked_account(session_attributes)
+    try:
+        access_token = event['session']['user']['accessToken']
+    except KeyError:
+        jsonify(build_linked_account({}))
 
     if event['session']['new']:
         on_session_started(
@@ -29,7 +36,7 @@ def alexa():
     if event['request']['type'] == 'LaunchRequest':
         response = on_launch(event['request'], event['session'])
     elif event['request']['type'] == 'IntentRequest':
-        response = on_intent(event['request'], event['session'])
+        response = on_intent(event['request'], event['session'], access_token)
     elif event['request']['type'] == 'SessionEndedRequest':
         response = on_session_ended(event['request'], event['session'])
     return jsonify(**response)
@@ -40,19 +47,21 @@ def on_session_started(session_started_request, session):
 
 
 def on_launch(launch_request, session):
-    return get_welcome_response()
+    return welcome_response()
 
 
-def on_intent(intent_request, session):
+def on_intent(intent_request, session, access_token):
     # intent = intent_request['intent']
     intent_name = intent_request['intent']['name']
 
     if intent_name == 'GetBalance':
-        return get_balance()
+        return balance(access_token)
+    elif intent_name == 'SpentToday':
+        return spent_today(access_token)
     elif intent_name == 'LastTransaction':
-        return get_last_transaction()
+        return last_transaction(access_token)
     elif intent_name == 'AMAZON.HelpIntent':
-        return get_welcome_response()
+        return welcome_response()
     elif (intent_name == 'AMAZON.CancelIntent' or
           intent_name == 'AMAZON.StopIntent'):
         return handle_session_end_request()
@@ -69,13 +78,13 @@ def handle_session_end_request():
     card_title = 'Monzo: Thanks'
     speech_output = u'Thank you for using the Monzo Alexa skill.'
     should_end_session = True
-    response = build_speechlet_response(
+    response = build_speech_response(
         card_title, speech_output, None, should_end_session
     )
     return build_response({}, response)
 
 
-def get_welcome_response():
+def welcome_response():
     session_attributes = {}
     card_title = 'Monzo'
     speech_output = """Welcome to the Alexa Monzo skill. Ask for you balance or
@@ -83,89 +92,58 @@ def get_welcome_response():
     reprompt_text = """Please ask me for your balance or transaction history."""  # NOQA
     should_end_session = False
 
-    reponse = build_speechlet_response(
+    reponse = build_speech_response(
         card_title, speech_output, reprompt_text, should_end_session
     )
     return build_response(session_attributes, reponse)
 
 
-def get_balance():
+def balance(access_token):
     session_attributes = {}
-    card_title = 'Your Monzo balance'
+    card_title = 'Monzo: Your balance'
     reprompt_text = ''
     should_end_session = False
 
-    balance = 100
-
-    speech = unicode('Your Monzo balance is £{0}.'.format(balance), 'utf-8')
-    response = build_speechlet_response(
+    balance = get_balance(access_token)
+    speech = unicode('Your Monzo balance is {0}.'.format(balance), 'utf-8')
+    response = build_speech_response(
         card_title, speech, reprompt_text, should_end_session
     )
     return build_response(session_attributes, response)
 
 
-def get_last_transaction():
+def spent_today(access_token):
     session_attributes = {}
-    card_title = 'Monzo: Last transaction'
+    card_title = 'Monzo: Spent Today'
     reprompt_text = ''
     should_end_session = False
 
-    amount = 100
-
-    speech_output = u'Your last transaction was for £{0}'.format(amount)
-    response = build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session
+    spent_today = get_spent_today(access_token)
+    speech = unicode('Your Monzo balance is {0}.'.format(spent_today), 'utf-8')
+    response = build_speech_response(
+        card_title, speech, reprompt_text, should_end_session
     )
     return build_response(session_attributes, response)
 
 
-def build_speechlet_response(title, output, reprompt_text, should_end_session):
-    return {
-        'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
-        },
-        'card': {
-            'type': 'Simple',
-            'title': title,
-            'content': output
-        },
-        'reprompt': {
-            'outputSpeech': {
-                'type': 'PlainText',
-                'text': reprompt_text
-            }
-        },
-        'shouldEndSession': should_end_session
-    }
+def last_transaction(access_token):
+    session_attributes = {}
+    card_title = 'Monzo: Your last transaction'
+    reprompt_text = ''
+    should_end_session = False
 
+    last_transaction = get_last_transaction(access_token)
+    amount = last_transaction['amount']
+    when = last_transaction['when']
+    merchant = last_transaction['merchant']
+    speech_output = u'Your last transaction was for {0} {1} at {2}'.format(
+        amount, when, merchant
+    )
 
-def build_response(session_attributes, speechlet_response):
-    return {
-        'version': '1.0',
-        'sessionAttributes': session_attributes,
-        'response': speechlet_response
-    }
-
-
-def build_linked_account(session_attributes):
-    return {
-        "version": "1.0",
-        "sessionAttributes": session_attributes,
-        "response": {
-            "outputSpeech": {
-                "type": "PlainText",
-                "text": """
-                    You must have a Monzo account to use this skill.
-                    Please use the Alexa app to link your Amazon account with
-                     your Monzo Account."""
-            },
-            "card": {
-                "type": "LinkAccount"
-            },
-            "shouldEndSession": False
-        }
-    }
+    response = build_speech_response(
+        card_title, speech_output, reprompt_text, should_end_session
+    )
+    return build_response(session_attributes, response)
 
 
 if __name__ == '__main__':
---
+++ monzo_skill/utils.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+
+from dateutil import parser
+
+from monzo import MonzoClient
+
+
+def get_balance(access_token):
+    monzo = MonzoClient(access_token=access_token)
+    monzo.update_tokens()
+    balance = monzo.get_balance()
+    return money_to_string(balance['balance'], balance['currency'])
+
+
+def get_spent_today(access_token):
+    monzo = MonzoClient(access_token=access_token)
+    monzo.update_tokens()
+    balance = monzo.get_balance()
+    return money_to_string(balance['spend_today'], balance['currency'])
+
+
+def get_last_transaction(access_token):
+    monzo = MonzoClient(access_token=access_token)
+    monzo.update_tokens()
+    # Should limit transactions to the last month
+    transaction = monzo.list_transactions(limit=100000)[::-1][0]
+
+    amount = money_to_string(transaction['amount'], transaction['currency'])
+    when = date_to_speech(transaction['created'])
+    merchant = transaction['merchant']['name']
+    return {
+        'amount': amount,
+        'when': when,
+        'merchant': merchant,
+    }
+
+
+def date_to_speech(date_string):
+    compare_time = parser.parse(date_string).replace(tzinfo=None)
+    days_ago = (datetime.today() - compare_time).replace(tzinfo=None).days
+    if days_ago == 0:
+        return 'today'
+    if days_ago == 1:
+        return 'yesterday'
+    if days_ago == 7:
+        return '1 week ago'
+    if days_ago > 31:
+        return 'over a month ago'
+    return '{0} days ago'.format(days_ago)
+
+
+def money_to_string(int_money, currency):
+    if int_money < 0:
+        int_money = int_money * -1
+    decimal_money = format(float(int_money) / float(100), '.2f')
+    currency_symbol_mapping = {
+        'GBP': u'£',
+        'USD': u'$',
+        'EUR': u'€',
+    }
+    currency_symbol = currency_symbol_mapping[currency]
+    return u'{0}{1}'.format(currency_symbol, decimal_money)
+
+
+def build_speech_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': title,
+            'content': output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
+
+
+def build_linked_account(session_attributes):
+    return {
+        "version": "1.0",
+        "sessionAttributes": session_attributes,
+        "response": {
+            "outputSpeech": {
+                "type": "PlainText",
+                "text": """
+                    You must have a Monzo account to use this skill.
+                    Please use the Alexa app to link your Amazon account with
+                     your Monzo Account."""
+            },
+            "card": {
+                "type": "LinkAccount"
+            },
+            "shouldEndSession": False
+        }
+    }
